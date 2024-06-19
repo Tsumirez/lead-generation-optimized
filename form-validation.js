10e22ae56e979ea413f1cce1555cb3df96462d0f
Zef <tsumirez@zoho.com>
2024-06-19 07:22:58 +0200

    Form validation mechanic developed. Still needs proper adjustments to real needs

---
+++ form-validation.js
@@ -0,0 +1,12 @@
+// let form = document.getElementById("contact-form");
+// // form.noValidate=true;
+// form.children[1].addEventListener("blur", validate);
+
+// function validate(e) {
+//     if(this.validity.valueMissing) {
+//         console.log('brak danych')
+//         this.setCustomValidity("zonk");
+//     }
+
+    // this.reportValidity();
+// }
